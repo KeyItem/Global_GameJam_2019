053a735ad05918d89fec63a5c5bdf861f67022c3
Matthew <MatthewFerreira514@gmail.com>
2019-01-25 22:22:04 -0500

    Implemented Player Input and Player Movement

---
+++ GlobalGamJam2019/Assets/Scripts/Entity/EntityController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour
+{
+    [Header("Entity Input Attributes")] public EntityInput input;
+
+    [Space(10)] public InputInfo inputValues;
+
+    [Space(10)] public bool isCapturingInput = true;
+
+    [Header("Entity Movement Attributes")] public EntityMovement[] movement;
+
+    [Space(10)] public bool isCapturingMovement = true;
+
+    private void Start()
+    {
+        InitializeController();
+    }
+
+    private void Update()
+    {
+        UpdateController();
+    }
+
+    private void InitializeController()
+    {
+        input = GetComponent<EntityInput>();
+        movement = GetComponentsInChildren<EntityMovement>();
+    }
+
+    private void UpdateController()
+    {
+        if (isCapturingInput)
+        {
+            ManageInput();
+        }
+
+        if (isCapturingMovement)
+        {
+            ManageMovement();
+        }
+    }
+
+    private void ManageInput()
+    {
+        input.GetInput();
+
+        inputValues = input.ReturnInput();
+    }
+
+    private void ManageMovement()
+    {
+        for (int i = 0; i < movement.Length; i++)
+        {
+            movement[i].Move(inputValues);
+        }
+    }
+}
---
+++ GlobalGamJam2019/Assets/Scripts/Entity/EntityMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement : MonoBehaviour
+{
+    [Header("Entity Movement Attributes")] public EntityMovementAttributes movementAttributes;
+
+    private Vector2 lastDirection = Vector2.down;
+    
+    public void Move(InputInfo input)
+    {
+        Vector2 newMovement = ReturnMovementVector(ReturnInputMovementVector(input));
+        
+        transform.Translate(newMovement * Time.deltaTime);
+
+        lastDirection = newMovement;
+    }
+
+    public Vector2 ReturnInputMovementVector(InputInfo input)
+    {
+        float xAxisRaw = input.ReturnCurrentAxis("HorizontalAxis").axisValue;
+        float yAxisRaw = input.ReturnCurrentAxis("VerticalAxis").axisValue;
+        
+        return new Vector2(xAxisRaw, 0);
+    }
+
+    public Vector2 ReturnMovementVector(Vector2 inputVector)
+    {
+        if (inputVector.magnitude > 1)
+        {
+            inputVector.Normalize();
+        }
+        
+        Vector2 newMovementVector = (inputVector  + movementAttributes.ReturnOngoingSpeed()).normalized;
+        
+        return newMovementVector * movementAttributes.movementSpeed;
+    }
+}
+
+[System.Serializable]
+public struct EntityMovementAttributes
+{
+    [Header("Entity Movement Speed")] public float movementSpeed;
+    
+    [Header("Entity Ongoing Movement Speed")]
+    public float ongoingSpeed;
+
+    [Space(10)] public Vector2 ongoingDirection;
+
+    public Vector2 ReturnOngoingSpeed()
+    {
+        return ongoingDirection * ongoingSpeed;
+    }
+}
